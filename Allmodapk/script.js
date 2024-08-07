b739eebfef3a4add0f8d54aef64d7664556edfdf
hasnain218 <muazzamh504@gmail.com>
2024-08-07 17:39:02 +0500

    Add files via upload

---
+++ Allmodapk/script.js
@@ -0,0 +1,7 @@
+// function swapContents() {
+//     var box1 = document.getElementById('box1');
+//     var box2 = document.getElementById('box2');
+//     var temp = box1.innerHTML;
+//     box1.innerHTML = box2.innerHTML;
+//     box2.innerHTML = temp;
+// }
